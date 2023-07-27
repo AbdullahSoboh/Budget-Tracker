d8b2cef34f6e395212db5f2cae209b29082f9eb3
Abdullah Soboh <abdullahsoboh@cmail.carleton.ca>
2023-07-26 22:08:46 -0400

    Improving Visuals and Creating a Like button

--- namer_app_flutter_practice/lib/main.dart
+++ namer_app_flutter_practice/lib/main.dart
@@ -32,7 +32,19 @@ class MyAppState extends ChangeNotifier {
     current = WordPair.random();
     notifyListeners();
   }
-}
+
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+    }
+  }
+
 
 class MyHomePage extends StatelessWidget {
   @override
@@ -40,19 +52,39 @@ class MyHomePage extends StatelessWidget {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
-    return Scaffold(
-      body: Column(
-        children: [
-          Text('A random AWESOME idea:'),
-          BigCard(pair: pair),
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
 
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            },
-            child: Text('Next'),
-          ),
-        ],
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(pair: pair),
+            SizedBox(height: 10),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children:[
+            
+                ElevatedButton(
+                  onPressed: () {
+                    appState.getNext();
+                  },
+                  child: Text('Next'),
+                ),
+                SizedBox(width: 10);
+                ElevatedButton.icon(onPressed: () {
+                  appState.toggleFavorite();
+                }, icon: Icon)
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -69,12 +101,12 @@ class BigCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final style = theme.textTheme.displayMedium!.copyWith(color: theme.colorScheme.onPrimary,); 
+    final style = theme.textTheme.displayMedium!.copyWith(color: theme.colorScheme.onSecondary).copyWith(decoration: TextDecoration.underline); 
     return Card(
       color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Text(pair.asLowerCase, style: style),
+        child: Text(pair.asLowerCase, style: style, semanticsLabel: "${pair.first} ${pair.second}"),
       ),
     );
   }
