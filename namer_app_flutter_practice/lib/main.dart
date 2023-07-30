7959f943981db7578f9ea6fcd9180796aaa85367
Abdullah Soboh <abdullahsoboh@cmail.carleton.ca>
2023-07-30 01:47:03 -0400

    Updates

--- namer_app_flutter_practice/lib/main.dart
+++ namer_app_flutter_practice/lib/main.dart
@@ -1,4 +1,4 @@
-//Abdullah Soboh F
+//Abdullah Soboh Flutter Project
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
